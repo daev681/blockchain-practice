1c64e70ff86e8952429dca6bfe023c9d2b97d0c0
Park DaeHeon <wow1186@naver.com>
2025-04-03 11:13:52 +0900

    pos 연습

--- pratice-pow/main.ts
+++ pratice-pow/main.ts
@@ -6,7 +6,6 @@ console.log("Genesis Block:", myChain.chain);
 
 const block1 = new Block(1, Date.now(), "Transaction 1", myChain.getLatestBlock().hash);
 myChain.addBlock(block1);
-
 const block2 = new Block(2, Date.now(), "Transaction 2", myChain.getLatestBlock().hash);
 myChain.addBlock(block2);
 
